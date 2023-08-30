86e705c9119a5b9231727851ec942958069297e0
im2ho <84431276+im2ho@users.noreply.github.com>
2023-08-30 17:51:38 +0900

    if/switch

--- chap01_Variable/src/com/kh/variable/VariablePractice2.java
+++ chap01_Variable/src/com/kh/variable/VariablePractice2.java
@@ -2,7 +2,7 @@ package com.kh.variable;
 
 import java.util.Scanner;
 
-public class Scannerexme {
+public class VariablePractice2 {
 	
 	public static void main(String[] args) {
 		//키보드에서 정수 두 개를 입력 받아, 두 수의 합, 차, 곱, 나누기 한 몫과 나머지 출력
@@ -17,10 +17,18 @@ public class Scannerexme {
 		System.out.print("두 번째 정수를 입력하세요 : ");
 		inum2 = sc.nextInt();
 		
+		System.out.print("더하기 결과: ");
+		System.out.println(inum1 + inum2);
+		
+		System.out.print("빼기 결과: ");
 		System.out.println(inum1 - inum2);
+
+		System.out.print("곱하기 결과: ");
+		System.out.println(inum1 * inum2);
+
+		System.out.print("나누기 결과: ");
+		System.out.print(inum1 / inum2);
+
 	}
 	
-	
-	
-
 }
