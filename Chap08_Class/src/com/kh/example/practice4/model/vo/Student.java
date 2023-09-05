92b3fc5b17a59ad32c402abb2793d56469cb9733
im2ho <84431276+im2ho@users.noreply.github.com>
2023-09-05 09:49:29 +0900

    ClassPractice

--- Chap08_Class/src/com/kh/example/practice4/model/vo/Student.java
+++ Chap08_Class/src/com/kh/example/practice4/model/vo/Student.java
@@ -1,24 +1,29 @@
 package com.kh.example.practice4.model.vo;
 
 public class Student {
+	
 	//1.멤버변수
+	
 	int grade;
 	int classroom;
 	String name;
 	double height;
 	char gender;
 	
-	//2.생산자
-	public Student(int grade, int classroom, String name, double height, char gender) { //학생정보 초기화
-		this.grade = grade;
-		this.classroom = classroom;
-		this.name = name;
-		this.height = height;
-		this.gender = gender;	
+	//2.생산자 Student()
+	
+	public Student() { 
+		// 초기화 블럭을 이용해 각 학생정보 필드 초기화 (초기화블럭{})
+		grade = 3;
+		classroom = 2;
+		name = "최새정";
+		height = 168.3;
+		gender = '여';
 	}
 	
 	
-	//3.메서드
+	//3.출력 메서드 + information() : void
+	
 	public void information() {
 		System.out.println("< " + name + " 학생 정보 >\n" );
 		System.out.println(grade + "학년 " + classroom + "반");
@@ -27,4 +32,3 @@ public class Student {
 	}
 
 }
-
